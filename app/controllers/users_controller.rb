56a546306562a42e2af159de7cf44c8922062553
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-12-03 13:21:49 +0000

    before_submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -11,14 +11,20 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
 
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -28,10 +34,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
+  def is_matching_login_user
+    user_id = params[:id].to_i
+   login_user_id = current_user.id
+    if(user_id != login_user_id)
       redirect_to user_path(current_user)
     end
   end
+
 end
