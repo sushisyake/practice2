1a825901224d49baa4a359672df0d076b0c73e21
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-12-06 06:34:23 +0000

    add_search

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   devise_for :users
   
-  resources :users do
+  resources :users , only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create,:destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
